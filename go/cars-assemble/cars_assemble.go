d8feb670b591b1c2d944f54dcf5c77e8ea5604a1
Yogender Singh <ys251294@gmail.com>
2025-05-31 08:22:19 +0530

    solution exercise

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,18 +1,25 @@
 package cars
 
+const CostPerIndividualCar = 10000
+const CostPerGroup = 95000
+
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	//panic("CalculateWorkingCarsPerHour not implemented")
+	return float64(productionRate) * successRate / 100
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	//panic("CalculateWorkingCarsPerMinute not implemented")
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	//panic("CalculateCost not implemented")
+	return uint((carsCount / 10 * CostPerGroup) + (carsCount % 10 * CostPerIndividualCar))
+
 }
