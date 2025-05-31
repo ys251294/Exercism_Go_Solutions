df72726e640d0c3721881e19212081487bfa57d6
Yogender Singh <ys251294@gmail.com>
2025-05-31 19:01:51 +0530

    solution 2 exercise

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -3,11 +3,8 @@ package twofer
 import "fmt"
 
 func ShareWith(name string) string {
-	var nameWithDefault string
 	if name == "" {
-		nameWithDefault = "you"
-	} else {
-		nameWithDefault = name
+		name = "you"
 	}
-	return fmt.Sprintf("One for %v, one for me.", nameWithDefault)
+	return fmt.Sprintf("One for %v, one for me.", name)
 }
