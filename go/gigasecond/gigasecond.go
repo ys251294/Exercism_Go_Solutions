94cf604a3d8989aab1f65f7f721273df0787555e
Yogender Singh <ys251294@gmail.com>
2025-05-31 21:56:06 +0530

    downloaded exercise

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,9 @@
+// Package gigasecond provide AddGigaseconds methods to manipulate time
+package gigasecond
+
+import "time"
+
+// AddGigasecond add 1000 million seconds to give time.
+func AddGigasecond(t time.Time) time.Time {
+	return t
+}
