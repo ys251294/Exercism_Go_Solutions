c5ef5087883ba7302363b70c0476f289fc41e145
Yogender Singh <ys251294@gmail.com>
2025-06-01 10:49:42 +0530

    solution exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,22 @@
 package hamming
 
+import (
+	"errors"
+)
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	//panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return -1, errors.New("strings are not the same length")
+	}
+	s1 := []rune(a)
+	s2 := []rune(b)
+
+	hammingDistance := 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			hammingDistance++
+		}
+	}
+	return hammingDistance, nil
 }
